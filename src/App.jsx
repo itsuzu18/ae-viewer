554ccac67456ea8594a7b79321cd5c1b04683dab
itsuzu18 <anggadika346@gmail.com>
2026-06-08 08:59:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { Canvas } from "@react-three/fiber"
+import {
+  Float,
+  OrbitControls,
+  useGLTF,
+} from "@react-three/drei"
+
+function Logo() {
+  const { scene } = useGLTF("/ae-logo.glb")
+
+  return (
+    <Float
+      speed={2}
+      rotationIntensity={0.1}
+      floatIntensity={0.3}
+    >
+      <primitive
+        object={scene}
+        scale={0.5}
+      />
+    </Float>
+  )
+}
+
+export default function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: "transparent",
+      }}
+    >
+      <Canvas
+        gl={{ alpha: true }}
+        camera={{
+          position: [0, 0, 8],
+          fov: 45,
+        }}
+      >
+        <ambientLight intensity={2} />
+
+        <hemisphereLight
+          intensity={1}
+        />
+
+        <directionalLight
+          position={[3, 3, 3]}
+          intensity={2}
+        />
+
+        <directionalLight
+          position={[-3, -2, 2]}
+          intensity={1}
+        />
+
+        <Logo />
+
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          enableDamping={true}
+          dampingFactor={0.08}
+          rotateSpeed={0.8}
+          autoRotate
+          autoRotateSpeed={1}
+        />
+      </Canvas>
+    </div>
+  )
+}
